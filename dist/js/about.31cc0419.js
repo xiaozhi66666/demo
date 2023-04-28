e883c4d51a08b3f79804e581b6b68f7e8fe5390a
zz <1871490331@qq.com>
2023-04-28 22:23:46 +0800

    message

---
+++ dist/js/about.31cc0419.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdemo"]=self["webpackChunkdemo"]||[]).push([[443],{399:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var e=function(){var n=this;n._self._c;return n._m(0)},s=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],a=u(1),l={},r=(0,a.Z)(l,e,s,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.31cc0419.js.map
